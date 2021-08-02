25e07bc17f8d854cdfdf60279e24f07369c31354
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-08-02 11:47:34 +0600

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,37 @@
+
+//Post Increment
+var a = 10;
+
+var result = a++;
+
+console.log(a);
+console.log(result);
+
+//Pre Increment
+var a = 10;
+
+var result = ++a;
+
+console.log(a);
+console.log(result);
+
+
+//Post Decrement
+
+var a = 10;
+
+var result = a--;
+
+console.log(a);
+console.log(result);
+
+
+//Pre Decrement
+
+var a = 10;
+
+var result = --a;
+
+console.log(a);
+console.log(result);
+
